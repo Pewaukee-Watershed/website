84304dff38d89a2699509f8ab1e227d829b3b682
KhajasJS <pararaj25@pewaukeeschools.org>
2021-01-07 19:19:19 +0000

    Title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,12 @@
-export default (): JSX.Element => <h1>Hi</h1>
+import Head from 'next/head'
+
+const App = (): JSX.Element => (
+    <>
+        <Head>
+            <title>Pewaukee Watershed</title>
+        </Head>
+        <h1>Hi</h1>
+    </>
+)
+
+export default App
